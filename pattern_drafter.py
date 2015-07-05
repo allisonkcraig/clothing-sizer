17d3d8c6fdf44de41f4cffd330a5efee663aeaea
Allison  Craig <allison@craig.net>
2015-07-04 21:11:09 -0700

    update css and add python file. start dictionary databse.

---
+++ pattern_drafter.py
@@ -0,0 +1,21 @@
+waist = float(raw_input("Enter Waist Measurement in inches"))
+hip = float(raw_input("Enter Hip Measurement in inches"))
+bust = float(raw_input("Enter Waist Measurement in inches"))
+
+old_navy_tops_num = {'zero': {'chest': 1, 'waist': 2}, 
+					 'one': {'chest': 1, 'waist': 2},
+					 'two': {'chest': 1, 'waist': 2},
+					 'four': {'chest': 1, 'waist': 2},
+					 'six': {'chest': 1, 'waist': 2},
+					 'eight': {'chest': 1, 'waist': 2},
+					 'ten': {'chest': 1, 'waist': 2},
+					 'twelve': {'chest': 1, 'waist': 2},
+					 'fourteen': {'chest': 1, 'waist': 2},
+					 'sixteen': {'chest': 1, 'waist': 2},
+					 'eighteen': {'chest': 1, 'waist': 2},
+					 'twenty': {'chest': 1, 'waist': 2},
+					 'twentytwo': {'chest': 1, 'waist': 2},
+					 'twentyfour': {'chest': 1, 'waist': 2},
+					 'twentysix': {'chest': 1, 'waist': 2},
+					 'twentyeight': {'chest': 1, 'waist': 2},
+					 'thirty': {'chest': 1, 'waist': 2},}
